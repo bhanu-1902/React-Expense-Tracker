c6239b45c6879c490c242d8ac6f6b2707a7a14a1
Harshit Bhanushali <harshit.21810372@viit.ac.in>
2021-07-23 09:01:02 +0530

    Split Components into smaller Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -26,26 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[3].date}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
@@ -7,7 +8,7 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
